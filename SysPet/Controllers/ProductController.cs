e0924b2e54bccce0e91ef852bbff959c8b1256e4
Rutilo <rugalindo@mundonectar.com>
2023-10-20 00:31:46 -0600

    Se agregan controladores , vistas de producto y ajuste de data

---
+++ SysPet/Controllers/ProductController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SysPet.Data;
+using SysPet.Models;
+
+namespace SysPet.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductsData productsData;
+        public ProductController()
+        {
+            productsData = new ProductsData();
+        }
+
+
+        // GET: ProductController
+        public async Task<ActionResult> Index()
+        {
+            var products = await productsData.GetProducts();
+            return View(products);
+        }
+
+        // GET: ProductController/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            var product = await productsData.GetProduct(id);
+            return View(product);
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ProductosViewModel model)
+        {
+            try
+            {
+                var result = productsData.CreateProduct(model);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public async Task<ActionResult> Update(int id)
+        {
+            var product = await productsData.GetProduct(id);
+            return View(product);
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Update(int id, ProductosViewModel model)
+        {
+            try
+            {
+                var product = await productsData.GetProduct(id);
+                if (product == null) { RedirectToAction(nameof(Index)); }
+
+                if (model == null) { RedirectToAction(nameof(Index)); }
+
+                if(!ModelState.IsValid) { RedirectToAction(nameof(Index));  }
+
+                var result = productsData.UpdateProduct(model, id);
+                
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        [HttpGet]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var product = await productsData.GetProduct(id);
+            return View(product);
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, ProductosViewModel model)
+        {
+            try
+            {
+                if (model == null) { RedirectToAction(nameof(Index)); }
+                var result = productsData.DeleteProduct(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                return View();
+            }
+        }
+    }
+}
--- SysPet/Data/ProductsData.cs
+++ SysPet/Data/ProductsData.cs
@@ -11,32 +11,67 @@ namespace SysPet.Data
 
         public ProductsData()
         {
-            connection = new SqlConnection();
-            connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Pets;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            
+            connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Pets;Integrated Security=True;Connect Timeout=30;Encrypt=False;";
+            connection = new SqlConnection(connectionString);
         }
 
-        public async Task<List<ProductosViewModel>> GetProducts()
+        public async Task<IEnumerable<ProductosViewModel>> GetProducts()
         {
-            var sql = "Select * FROM Productos";
-            var productos = await connection.QueryAsync<ProductosViewModel>(sql);
+            try
+            {
+                var sql = @$"SELECT [IdProducto],[Nombre],[FechaIngreso],[Proveedor],[Cantidad],[Stock],[PrecioUnitario],[PrecioSugerido],[Descripcion],[FechaVencimiento],[Estado]
+                        FROM [dbo].[Productos]";
 
-            return productos.ToList();
+                connection.Open();
+                var productos = await connection.QueryAsync<ProductosViewModel>(sql);
+                connection.Close();
+                return productos;
+            }
+            catch (Exception ex)
+            {
+                return new List<ProductosViewModel>();
+            }
+            
         }
 
-        public async Task<List<ProductosViewModel>> GetProducts(int idProducto)
+        public async Task<ProductosViewModel> GetProduct(int idProducto)
         {
-            var sql = "Select * FROM Productos WHERE idProducto = @idProducto";
-            var productos = await connection.QueryAsync<ProductosViewModel>(sql, new { idProducto });
-
-            return productos.ToList();
+            var sql = @$"SELECT [IdProducto],[Nombre],[FechaIngreso],[Proveedor],[Cantidad],[Stock],[PrecioUnitario],[PrecioSugerido],[Descripcion],[FechaVencimiento],
+                                [Estado]  FROM [Pets].[dbo].[Productos] WHERE idProducto = @idProducto";
+            connection.Open();
+            var producto = await connection.QuerySingleAsync<ProductosViewModel>(sql, new { idProducto });
+            connection.Close();
+            return producto;
         }
 
         public int CreateProduct(ProductosViewModel producto)
         {
             var sql = @$"INSERT INTO Productos (Nombre, FechaIngreso, Proveedor, Cantidad, Stock, PrecioUnitario, PrecioSugerido, Descripcion, FechaVencimiento, Estado)
-            VALUES ('${producto.Nombre}', '${producto.FechaIngreso}', '${producto.Proveedor}', ${producto.Cantidad}, ${producto.Stock}, 
-                    ${producto.PrecioUnitario}, ${producto.PrecioSugerido}, '${producto.Descripcion}', '${producto.FechaVencimiento}', 1);";
+            VALUES ('${producto.Nombre}', '${FormatDate(producto.FechaIngreso)}', '${producto.Proveedor}', ${producto.Cantidad}, ${producto.Stock}, 
+                    ${producto.PrecioUnitario}, ${producto.PrecioSugerido}, '${producto.Descripcion}', '${FormatDate(producto.FechaVencimiento)}', 1);";
+            connection.Open();
             var result = connection.Execute(sql);
+            connection.Close();
+
+            return result;
+        }
+
+        public int UpdateProduct(ProductosViewModel producto, int idProducto)
+        {
+            var estado = producto.Estado ? 1 : 0;
+            var sql = @$"UPDATE Productos SET Nombre='{producto.Nombre}',
+                                Cantidad={producto.Cantidad}, 
+                                Stock={producto.Stock},
+                                PrecioUnitario={producto.PrecioUnitario},
+                                PrecioSugerido={producto.PrecioSugerido},
+                                Descripcion='{producto.Descripcion}',
+                                FechaVencimiento='{FormatDate(producto.FechaVencimiento)}',
+                                Estado={estado }
+                        WHERE idProducto = @idProducto";
+            connection.Open();
+            var result = connection.Execute(sql, new { idProducto });
+            connection.Close();
 
             return result;
         }
@@ -44,10 +79,16 @@ namespace SysPet.Data
         public int DeleteProduct(int idProducto)
         {
             var sql = $"DELETE FROM Productos WHERE IdProducto = @idProducto;";
+            connection.Open();
             var result = connection.Execute(sql, new { idProducto});
+            connection.Close();
 
             return result;
         }
 
+        private string FormatDate(DateTime date)
+        {
+            return $"{date.Year}-{date.Month}-{date.Day}";
+        }
     }
 }
